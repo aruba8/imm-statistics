4206ed46e042eb81676f52eeb4bce1c43b948fa5
Erik Khalimov <biomaks@gmail.com>
2014-11-10 22:07:58 +0300

    removed dependency

--- app/sessions.py
+++ app/sessions.py
@@ -5,7 +5,6 @@ import random
 import string
 import hashlib
 
-from bson.objectid import ObjectId
 from utils.logger import Logger
 
 logger = Logger()
@@ -16,19 +15,6 @@ from app import db
 
 
 class Sessions:
-
-    def get_session(self, session_id):
-        # this may fail because the string may not be a valid bson objectid
-        try:
-            _id = ObjectId(session_id)
-        except:
-            log.info("bad sessionid passed in")
-            return None
-        session = SessionModel.objects.get(id=_id)
-
-        log.info("returning a session or none")
-        return session
-
     def start_session(self, user_id):
         # session = {'username': user_id}
         try:
@@ -36,16 +22,6 @@ class Sessions:
         except:
             log.error("Unexpected error on start_session:")
             return -1
-
-    # will send a new user session by deleting from sessions table
-    def end_session(self, session_id):
-        # this may fail because the string may not be a valid bson objectid
-        try:
-            SessionModel.objects(id=ObjectId(session_id)).delete()
-        except:
-            log.warn("Couldn't delete session!!!")
-
-            # validates the login, returns True if it's a valid user login. false otherwise
 
     def validate_login(self, username, password):
         user = User.query.filter_by(username=username).first()
@@ -78,27 +54,6 @@ class Sessions:
         for i in range(5):
             salt = salt + random.choice(string.ascii_letters)
         return salt
-
-    def login_check(self, cookie):
-
-        if cookie is None:
-            log.info("no cookie...")
-            return None
-
-        else:
-            session_id = self.check_secure_val(cookie)
-
-            if session_id is None:
-                log.info("no secure session_id")
-                return None
-
-            else:
-                # look up username record
-                session = self.get_session(session_id)
-                if session is None:
-                    return None
-
-        return session['username']
 
     def check_secure_val(self, h):
         val = h.split('|')[0]
