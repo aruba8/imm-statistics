0ae49f84f8a0db77e342213497338c0a57ca96a4
Erik Khalimov <biomaks@gmail.com>
2015-04-11 10:14:20 +0300

    fixed error handling during login

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -1,3 +1,5 @@
+from app.models.user import User
+
 __author__ = 'erik'
 from flask_wtf import Form
 from wtforms.fields import StringField, PasswordField
@@ -7,4 +9,21 @@ from wtforms.validators import DataRequired
 class LoginForm(Form):
     login = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+        self.user = None
+
+    def validate(self):
+        rv = Form.validate(self)
+        if not rv:
+            return False
+
+        user = User.query.filter_by(username=self.login.data).first()
+        if user is None:
+            self.login.errors.append('Unknown user')
+            return False
+
+        self.user = user
+        return True
 
