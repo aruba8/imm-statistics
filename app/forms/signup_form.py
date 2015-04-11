5d80c3fdee228730eb7880cdd80a647297bd95a6
Erik Khalimov <biomaks@gmail.com>
2015-04-11 10:37:48 +0300

    email validation during login

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,3 +1,5 @@
+from app.models.user import User
+
 __author__ = 'erik'
 from flask_wtf import Form, RecaptchaField
 from wtforms.fields import StringField, PasswordField, SelectField
@@ -9,8 +11,8 @@ COUNTRIES = [country.alpha2 for country in countries.objects]
 
 def create_countries_dict():
     country_dict = []
-    for country in COUNTRIES:
-        country_dict.append((country, countries.get(alpha2=country).name))
+    for _country in COUNTRIES:
+        country_dict.append((_country, countries.get(alpha2=_country).name))
     return country_dict
 
 
@@ -21,6 +23,31 @@ class SignUpForm(Form):
     confirm = PasswordField('Confirm')
     country = SelectField(choices=create_countries_dict())
     recaptcha = RecaptchaField()
+
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+        self.user = None
+
+    def validate(self):
+        rv = Form.validate(self)
+
+        user = User.query.filter_by(username=self.login.data).first()
+        email = User.query.filter_by(email=self.email.data).first()
+
+        if user is not None:
+            self.login.errors.append('This username has already taken')
+            return False
+
+        if email is not None:
+            self.email.errors.append('This email is using for another account')
+
+
+
+        if not rv:
+            return False
+
+        self.user = user
+        return True
 
 
 
